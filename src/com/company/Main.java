e835abd26283f1bf3951185f27c0cd00ceb03f36
Yasmin Behi <ybehi0001@yahoo.com>
2019-04-17 15:55:45 -0400

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println( "Coast" + "    " + " Quality " + "     " + "Total");
+        System.out.println("$1,000.00" + "     " + "4" +"       " + "$4,000.00");
+        System.out.println("   $50.00" + "     " + "8" +"       " + "  $400.00");
+        System.out.println("________________________________");
+        System.out.println( " TOTAL :" + "    " +" 12       " + "$4,400.00");
+    }
+
+
+}
+
+
+
